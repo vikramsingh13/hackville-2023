613e76f72e28f1a1cd1981098a62979887409a03
Artem Gusev <artemgusev2100@gmail.com>
2023-02-11 16:08:36 -0500

    Folder setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,13 @@
 import { useState } from 'react'
+import Landing from './components/landing/Landing'
 import reactLogo from './assets/react.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App w-screen h-screen bg-red-700">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="">
+		<Landing/>
     </div>
   )
 }
---
+++ client/src/components/landing/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Landing() {
+  return (
+    <div className="w-screen h-screen">
+      <div className="w-screen h-12 flex-col justify-between items-center bg-orange-400">
+        <h1 className="text-center h-full">Potatotato</h1>
+      </div>
+      <div>
+      </div>
+    </div>
+  );
+}
