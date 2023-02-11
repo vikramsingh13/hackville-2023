6970959f30cfe137f4e17262bb849e76ab2f94b7
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-02-11 14:04:01 -0500

    cohere integration

--- controllers/promptController.js
+++ controllers/promptController.js
@@ -1,12 +1,28 @@
 const asyncHandler = require('express-async-handler');
+const cohere = require('cohere-ai');
+cohere.init(process.env.COHERE_API_KEY)
 //const promptModel = require('../models/promptModel') //todo
 
 //@desc Get data from co:here ai
 //@path GET /api/prompts
 //access PRIVATE
 const getCohereData = asyncHandler(async(req, res) => {
-    //todo
-    res.status(200).json({"message" : "This is a sample response."});
+    //model can be large or medium
+    //medium by default now
+    const model = "medium"
+    if(req.body.prompt){
+        cohere.init(process.env.COHERE_API_KEY);
+        const response = await cohere.generate({
+            model: model,
+            prompt: req.prompt,
+            max_tokens: 50,
+            temperature: 1,
+        });
+        res.status(200).json({"message" : response});
+    } else {
+        res.status(400)
+        throw new Error("Please add a prompt key with value to the request body json");
+    }
 })
 
 module.exports = { getCohereData }
