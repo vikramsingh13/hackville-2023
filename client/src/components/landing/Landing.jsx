bb14c64916557d9ccc6ec49f1f3642792f4e14a7
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-02-12 00:19:48 -0500

    fixed nav for landing

--- client/src/components/landing/Landing.jsx
+++ client/src/components/landing/Landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './Landing.css';
 
-export default function Landing() {
+export default function Landing({handleNavClick}) {
   return (
     <div id="container">
       <div id="wrapper">
@@ -10,22 +10,22 @@ export default function Landing() {
         <img className="prop third" src="/images/prop1.png" />
         <img className="prop2 fifth" src="/images/prop2.png" />
         <img className="prop2 sixth" src="/images/prop2.png" />
-        <a className="first" data-fixed="broken">
+        <a className="first" data-fixed="broken" onClick={handleNavClick}>
           <img className="building hover" src="/images/building1.png" />
           <div className="tag">Print()</div>
           <div className="tag2">FIX ME</div>
         </a>
-        <a className="second" data-fixed="broken">
+        <a className="second" data-fixed="broken" onClick={handleNavClick}>
           <img className="building smaller hover" src="/images/building2.png" />
           <div className="tag">I/O</div>
           <div className="tag2">FIX ME</div>
         </a>
-        <a className="third" data-fixed="broken">
+        <a className="third" data-fixed="broken" onClick={handleNavClick}>
           <img className="building hover" src="/images/building3.png" />
           <div className="tag">Vars</div>
           <div className="tag2">FIX ME</div>
         </a>
-        <a className="fourth" data-fixed="broken">
+        <a className="fourth" data-fixed="broken" onClick={handleNavClick}>
           <img className="building smaller hover" src="/images/building4.png" />
           <div className="tag">Dict</div>
           <div className="tag2">FIX ME</div>
