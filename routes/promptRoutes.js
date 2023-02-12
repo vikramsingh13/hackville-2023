6dc12a5ae169b098b0b0675a4e0b349b297c873b
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-02-11 20:04:12 -0500

    vercel debug

--- routes/promptRoutes.js
+++ routes/promptRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getCohereData, getDialogues} = require('../controllers/promptController');
 
 router.route('/prompts').get(getCohereData);
-router.route('/dialogues').get(getDialogues);
+//router.route('/dialogues').get(getDialogues);
 
 module.exports = router;
