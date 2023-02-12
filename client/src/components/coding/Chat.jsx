85b0c75e99c261a6bf2f68284032e7e432aea698
Artem Gusev <artemgusev2100@gmail.com>
2023-02-11 19:39:15 -0500

    More commit tasty - sexy

--- client/src/components/coding/Chat.jsx
+++ client/src/components/coding/Chat.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Chat({chat}) {
   return (
-    <div>{chat}</div>
+    <div className='w-full h-full bg-orange-300'>
+        <div className='flex justify-around h-full'>
+            {/* chat area with input*/}
+            <div className='w-2/3 h-full'>
+                
+            </div>
+            
+            {/* potato area */}
+            <div className='w-1/3 h-full'>
+                
+            </div>
+        </div>
+    </div>
   )
 }
--- client/src/components/coding/Console.jsx
+++ client/src/components/coding/Console.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 export default function Console() {
   return (
-    <div className='bg-black h-full text-green-600 text-mon font-mono'>
+    <div className='bg-black text-green-600 text-mon font-mono z-10'>
         <div className='w-full h-8 border-b border-green-600 flex justify-center items-center'>
             <p>TATA-TEC 2.0</p>
         </div>
-        <div className='w-full h-full'>
-            <textarea className='p-2 text-2xl w-full h-full resize-none bg-black max-w-none' 
+        <div className='w-full'>
+            <textarea className='p-2 text-2xl w-full resize-none bg-black max-w-none' 
             name="tatotext" id="tatotext" cols="30" rows="10"></textarea>
         </div>
-        <div className='bg-black w-full h-10 border-t border-green-600'>
+        <div className='bg-black w-full h-10 border-t border-green-600 z-10'>
             <div className='flex justify-end'>
-                <button className='bg-green-600 text-white p-2 pl-10 pr-10'>Run</button>
+                <button className='bg-green-600 text-white p-2 pl-10 pr-10 active:bg-green-300'>Run</button>
             </div>
         </div>
     </div>
