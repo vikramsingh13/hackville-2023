bdbf0cd864cde0920d17be0a74ec49c4dc8ee34b
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-02-11 23:06:49 -0500

    added landing page

--- client/src/components/landing/Landing.jsx
+++ client/src/components/landing/Landing.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import './Landing.css';
 
 export default function Landing() {
   return (
-    <div className="w-screen h-screen">
-      <div className="w-screen h-12 flex-col justify-between items-center bg-orange-400">
-        <h1 className="text-center h-full">Potatotato</h1>
-      </div>
-      <div>
+    <div id="container">
+      <div id="wrapper">
+        <div id="sky"></div>
+        <img className="prop first" src="/images/prop1.png" />
+        <img className="prop third" src="/images/prop1.png" />
+        <img className="prop2 fifth" src="/images/prop2.png" />
+        <img className="prop2 sixth" src="/images/prop2.png" />
+        <a className="first" data-fixed="broken">
+          <img className="building hover" src="/images/building1.png" />
+          <div className="tag">Print()</div>
+          <div className="tag2">FIX ME</div>
+        </a>
+        <a className="second" data-fixed="broken">
+          <img className="building smaller hover" src="/images/building2.png" />
+          <div className="tag">I/O</div>
+          <div className="tag2">FIX ME</div>
+        </a>
+        <a className="third" data-fixed="broken">
+          <img className="building hover" src="/images/building3.png" />
+          <div className="tag">Vars</div>
+          <div className="tag2">FIX ME</div>
+        </a>
+        <a className="fourth" data-fixed="broken">
+          <img className="building smaller hover" src="/images/building4.png" />
+          <div className="tag">Dict</div>
+          <div className="tag2">FIX ME</div>
+        </a>
       </div>
     </div>
   );
